2ae739372b4bf45080f850f429a2f5ecdb83b96a
Merzki <guforski@gmail.com>
2025-04-12 14:17:09 +0200

    projects component refactoring

--- src/app/pages/it/projects/projects.component.ts
+++ src/app/pages/it/projects/projects.component.ts
@@ -2,10 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  standalone: true
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'PaintAnalog (WPF)',
+      description: `A custom-built Paint analog made with WPF and MVVM.
+        Features include drawing tools, image & text manipulation, selection with confirmation, undo/redo, clipboard support,
+        transform handles (resizing, rotating), and a modular tool system. Built for responsive, precise user interaction.`,
+      link: 'https://github.com/Merzki/PaintAnalog'
+    },
+    {
+      title: 'Merzkiself (Angular)',
+      description: `Personal portfolio built with Angular. Responsive design, themed support, component-based structure,
+        and dynamic rendering through data-driven architecture. Showcases experience and dev approach.`,
+      link: 'https://github.com/Merzki/Merzkiself'
+    }
+  ];
 }
