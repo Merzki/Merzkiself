3e7f91b4c379e63a7306f5fd235d6f899bbdf163
Merzki <guforski@gmail.com>
2025-04-12 22:20:02 +0200

    add new it-section's navbar animation

--- src/app/pages/it/it.component.ts
+++ src/app/pages/it/it.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-it',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './it.component.html',
-  styleUrl: './it.component.scss'
+  styleUrls: ['./it.component.scss']
 })
-export class ItComponent {
+export class ItComponent implements OnInit {
+  currentRoute = '';
   
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(event => {
+        this.currentRoute = (event.urlAfterRedirects || event.url).replace('/', '');
+      });
+  }
+
+  ngOnInit() {
+    this.currentRoute = this.router.url.replace('/', '');
+  }
+
+  getLinkClass(route: string): string {
+    const isActive = this.currentRoute.includes(route);
+    const direction = route === 'skillset' ? 'from-right' : 'from-left';
+    return `${isActive ? 'active-link' : ''} ${direction}`;
+  }
 }
