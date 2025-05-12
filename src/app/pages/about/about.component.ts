c5a8b6eb10eef67f3031604b77a6db4b0f36330d
Merzki <guforski@gmail.com>
2025-05-12 22:10:03 +0200

    improve style adaptive properties

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -34,16 +34,16 @@ export class AboutComponent implements OnInit, OnDestroy {
     },
     {
       image: 'assets/imgs/me1.jpg',
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       image: 'assets/imgs/me1.jpg',
-      text: "Hi, I'm Anton, an 20-year-old programming enthusiast, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+      text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     }
   ];
 
   currentSlide = 0;
-  slideDuration = 5000; 
+  slideDuration = 4000; 
   intervalId: any;
   animationState: 'visible' | 'hidden' = 'hidden';
 
