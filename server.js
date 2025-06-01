1f112f7bc53401d51833e6a384de7da01f5aa6ae
Merzki <guforski@gmail.com>
2025-06-01 03:34:44 +0200

    minor fixes

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist/merzkiself/browser')));
 
-// Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist/merzkiself/browser/index.html'));
 });
 
-// Start the app by listening on the default port
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
